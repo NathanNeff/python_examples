acfa98c7ba1fb831ab70332c1c3b1f9b64bcf06b
Nathan Neff <nathan.neff@gmail.com>
2013-05-26 08:30:49 -0500

    extend and append and plus on lists

--- list.py
+++ list.py
@@ -1,15 +1,29 @@
 #!/bin/env python
 
 def main():
-    test_inserts()
+    test_inserts_and_appends()
     test_basic()
     test_sort()
 
-def test_inserts():
+def test_inserts_and_appends():
     another_list = [None] * 5
     another_list[0] = "Zero"
     another_list[4] = "Four"
     assert another_list == [ "Zero", None , None, None, "Four" ]
+
+    # Plus operator on lists (it ain't extend or append)
+    first_list = [ "a", "b" ]
+    second_list = [ "c", "d" ]
+    assert [ "a", "b", "c", "d" ] == first_list + second_list
+
+    extend_example = [ 1, 2 ]
+    extend_example.extend([ 3, 4 ])
+    assert [ 1, 2, 3, 4 ] == extend_example
+
+    append_example = [ 1, 2 ]
+    append_example.append([ 3, 4 ])
+    assert [ 1, 2, [ 3, 4 ] ] == append_example
+
 
 def test_basic():
     lst = [ 1, 2, 3 ]
