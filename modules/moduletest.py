d378a9ab80a96f49f5dfe6b30f1a2d10bcd4f3bd
Nathan Neff <nathan.neff@gmail.com>
2015-02-07 19:07:36 -0600

    Add function description?

--- modules/moduletest.py
+++ modules/moduletest.py
@@ -3,6 +3,7 @@ import natemod
 
 class Something(unittest.TestCase):
     def testSomething(self):
+        "This is the description of this function?"
         self.assertEqual("Foo",natemod.natefunc("Foo"))
 
 if __name__ == "__main__":
