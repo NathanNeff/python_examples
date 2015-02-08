427bf071f42a8946f7903408095b2228e916c51d
Nathan Neff <nathan.neff@gmail.com>
2015-02-07 19:03:20 -0600

    Add unit test for modules

---
+++ modules/moduletest.py
@@ -0,0 +1,9 @@
+import unittest
+import natemod
+
+class Something(unittest.TestCase):
+    def testSomething(self):
+        self.assertEqual(1,1)
+
+if __name__ == "__main__":
+    unittest.main()
