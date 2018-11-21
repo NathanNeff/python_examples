78afc937b31043c4d64387234f0e31e52ed33d58
Nathan Neff <nathan.neff@gmail.com>
2018-11-20 22:10:21 -0600

    Loop through commandline args
    Also fix print() statement for Python3 using
    2to3 tool

--- args.py
+++ args.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 import sys
 if len(sys.argv) > 1:
-    print sys.argv[1]
+    print(sys.argv[1])
+
+if len(sys.argv) == 1:
+    print(sys.argv[0])
+    sys.exit(1)
+
+for arg,idx in enumerate(sys.argv):
+    print(("arg {}: {}".format(arg,idx)))
+
+
