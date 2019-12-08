bcc6b01258058237ad3086d89a3e30c8687805de
Nathan Neff <nathan.neff@gmail.com>
2019-12-08 12:07:29 +0100

    Some regex stuff

--- re_test.py
+++ re_test.py
@@ -9,5 +9,17 @@ class SomeTester(unittest.TestCase):
         self.assertIsNotNone(re.search("1.", pgn))
         self.assertIsNotNone(re.search("\s+2.", pgn))
 
+    def test_another(self):
+        self.assertIsNotNone(re.search(".png", "foo.PNG", flags=re.IGNORECASE),
+                                      "Should be case-insensitive")
+        self.assertIsNone(re.search(".png$", "foo.PNG.txt", flags=re.IGNORECASE), 
+                                    "Should be case-insensitive")
+        self.assertIsNotNone(re.search(".png$", "foo.png.txtpng", flags=re.IGNORECASE), 
+                ".png should have period and be last")
+        self.assertIsNone(re.search("\.png$", "foo.png.txtpng", flags=re.IGNORECASE), 
+                ".png should have period and be last")
+        self.assertIsNone(re.search("\.png$", "foopng", flags=re.IGNORECASE), 
+                ".png should have period and be last")
+
 if __name__ == "__main__":
     unittest.main()
--- regex.py
+++ regex.py
@@ -4,13 +4,22 @@ tests = ["* This guy             :tag:again:              ",\
          "another :tag:",\
          "* That guy:This is not a tag          :invalid tag :validtag: "]
 
-print "Starting"
-for line in tests:
-    m = re.match('\*.*?(:.*:)\s*$', line)
-    if m:
-        for grp in m.groups():
-            for tag in grp.split(":"):
-                if len(tag) and not re.search('\s', tag):
-                    print "'" + tag + "'"
+print("Starting")
+if False:
+    for line in tests:
+        m = re.match('\*.*?(:.*:)\s*$', line)
+        if m:
+            for grp in m.groups():
+                for tag in grp.split(":"):
+                    if len(tag) and not re.search('\s', tag):
+                        print("'" + tag + "'")
 
-print "Done"
+tests = ["end of section**beginning"]
+for line in tests:
+    m = re.search("\S\*\*", line)
+    if m:
+        print("Yes: " + line)
+        for grp in m.groups():
+            print(grp)
+
+print("Done")
